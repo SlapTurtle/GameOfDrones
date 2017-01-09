c69a8654f4ee82b5642b61dfe5dce0347808ca95
SlapTurtle <michaeldkirkegaard@gmail.com>
2017-01-09 15:55:46 +0100

    ?

--- Test/src/UI.java
+++ Test/src/UI.java
@@ -1,4 +1,11 @@
+import java.awt.Point;
+import java.util.LinkedList;
 import java.util.UUID;
+
+import org.cmg.resp.behaviour.Agent;
+import org.cmg.resp.knowledge.Template;
+import org.cmg.resp.knowledge.Tuple;
+import org.cmg.resp.knowledge.ts.TupleSpace;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.*;
@@ -29,10 +36,13 @@ public class UI extends Application {
     public class GridDisplay {
 
         public static final double WINDOW_SIZE = 1000;
-        public static final int INIT_GRID_SIZE = 31;
+        public static final int INIT_GRID_SIZE = 49;
+
+        public final double initSize;
         
         public double gridSize;
         public double tileSize;
+        public double scale;
         public Base base;
         
         public Pane pane = new Pane();
@@ -40,26 +50,27 @@ public class UI extends Application {
 
         public GridDisplay() {
         	resizeGrid(INIT_GRID_SIZE);
+        	initSize = INIT_GRID_SIZE;
         }
         
         public GridDisplay(int gridSize) {
         	resizeGrid(gridSize);
+        	initSize = WINDOW_SIZE / gridSize;
         }
 
         public void resizeGrid(int n){
         	gridSize = n;
-        	tileSize = WINDOW_SIZE / gridSize;
-        	pane.setScaleX(tileSize/2);
-        	pane.setScaleY(tileSize/2);
+        	double newtileSize = WINDOW_SIZE / gridSize;
+        	if(tileSize != 0){
+        		scale = (newtileSize / tileSize);
+		    	pane.setScaleX(scale);
+		    	pane.setScaleY(scale);
+        	}
+        	else{
+        		scale = 1;
+        	}
+        	tileSize = newtileSize;
         }
-
-        /*private Shape createElement(int x, int y) {
-        	Box box = new Box(GAP);
-            Drone drone = new Drone(x,y,GAP/2);
-            if(x==1) box.setVisible(false);
-            return (x==2 && y ==2 || x==3 && y==3) ? drone : box;
-        }*/
-
     }
 
     @Override
@@ -68,40 +79,60 @@ public class UI extends Application {
         gridDisplay = new GridDisplay();
         Pane pane = gridDisplay.pane;
         Group display = gridDisplay.display;
-        pane.setPrefHeight(gridDisplay.WINDOW_SIZE);
-        pane.setPrefWidth(gridDisplay.WINDOW_SIZE);
-        Scene scene = new Scene(display, gridDisplay.WINDOW_SIZE, gridDisplay.WINDOW_SIZE);
+        pane.setPrefHeight(GridDisplay.WINDOW_SIZE);
+        pane.setPrefWidth(GridDisplay.WINDOW_SIZE);
+        Scene scene = new Scene(display, GridDisplay.WINDOW_SIZE-12, GridDisplay.WINDOW_SIZE-12);
         
-        World world = new World(GridDisplay.INIT_GRID_SIZE);
+        World world = new World(new Point(0,0), GridDisplay.INIT_GRID_SIZE);
 		Map map = new Map(world);
-		map.
-		
+		LinkedList<Tuple> items = map.RetrieveTuples();
+				
 		pane.getChildren().clear();
 		Base b = new Base(gridDisplay, 0, 0);
         gridDisplay.base = b;
         pane.getChildren().add(b);
         
-        for (int i = 0; i < gridDisplay.gridSize; i++) {
-            for (int j = 0; j < gridDisplay.gridSize; j++) {
-            	Shape c = null;
-            	switch (N[i][j]) {
-				case 1: c = new Gold(gridDisplay, i, j); break;
-				case 2: c = new Tree(gridDisplay, i,j); break;
-				case 3: //base = nothing
-				case 4: c = new Drone(gridDisplay, i,j); break;
-				}
-                if(c != null) {
-                	System.out.println(c.getClass().toString());
-                	pane.getChildren().add(c);
-                }
+        for(Tuple t : items){
+        	String str = t.getElementAt(String.class, 0);
+        	//System.out.println(str);
+        	int x = t.getElementAt(Integer.class, 1);
+        	int y = t.getElementAt(Integer.class, 2);
+        	Shape c = null;
+        	switch (str) {
+			case "GOLD": c = new Gold(gridDisplay, x, y); break;
+			case "TREE": c = new Tree(gridDisplay, x, y); break;
+			case "BASE": break;//base = nothing
+			case "WATER": c = new Drone(gridDisplay, x, y); break;
+			case "EXPDRONE" : c = new Drone(gridDisplay, x, y); break;
+			case "HARDRONE" : c = new Drone(gridDisplay, x, y); break;
+			}
+            if(c != null) {
+            	//System.out.println(c.getClass().toString());
+            	pane.getChildren().add(c);
             }
-        }
-        
+        }//*/       
         
         primaryStage.setResizable(false);
         primaryStage.setTitle("Test grid display");
         primaryStage.setScene(scene);
-        primaryStage.show();       
+        primaryStage.show(); 
+        
+        
+        try {
+        	Thread.sleep(2000);
+        	Platform.runLater(() -> gridDisplay.pane.getChildren().add(new Tree(gridDisplay, -1, 0)));
+			Thread.sleep(2000);
+			Platform.runLater(() -> gridDisplay.resizeGrid(69));
+			Thread.sleep(2000);
+			Platform.runLater(() -> gridDisplay.pane.getChildren().add(new Tree(gridDisplay, 1, 0)));
+			Thread.sleep(2000);
+			Platform.runLater(() -> gridDisplay.resizeGrid(89));
+			Thread.sleep(2000);
+			Platform.runLater(() -> gridDisplay.pane.getChildren().add(new Tree(gridDisplay, 0, 1)));
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+        
     }
 
     /**
@@ -115,32 +146,31 @@ public class UI extends Application {
     	public GridDisplay gridDisplay;
     	
     	public CoordinateShape(GridDisplay gridDisplay, int x, int y, Paint c){
-    		super(x,y,1,c);
+    		super(x,y,gridDisplay.tileSize/2,c);
+    		System.out.println(gridDisplay.tileSize/2);
     		this.gridDisplay = gridDisplay;
-    		if(!getClass().equals(Base.class)) {
-    			setX(x);
-        		setY(y);
-    		}
-    		else {
-    			setTranslateX(gridDisplay.WINDOW_SIZE / 2);
-    			setTranslateY(gridDisplay.WINDOW_SIZE / 2);
-    		}
+			setX(x);
+    		setY(y);
     	}
     	
     	public void setX(int x){
-    		double zero = gridDisplay.base.getTranslateX();
+    		this.setCenterX(x);
+    		this.setLayoutY(x);
+    		double zero = (gridDisplay.base == null) ? GridDisplay.WINDOW_SIZE/2 : gridDisplay.base.getTranslateX();
     		double offset = gridDisplay.tileSize * x;
     		double newX = zero + offset;
-    		setTranslateX(newX);
-    		this.setCenterX(x);
+    		System.out.println("x:"+x+" - os:"+offset+" - z:"+zero+" - nx:"+newX);
+    		setTranslateX(newX);    		
     	}
     	
     	public void setY(int y){
-    		double zero = gridDisplay.base.getTranslateY();
+    		this.setCenterY(y);
+    		this.setLayoutY(y);
+    		double zero = (gridDisplay.base == null) ? GridDisplay.WINDOW_SIZE/2 : gridDisplay.base.getTranslateY();
     		double offset = gridDisplay.tileSize * y;
     		double newY = zero + offset;
+    		//System.out.println("y:"+y+" - os:"+offset+" - z:"+zero+" - ny:"+newY);
     		setTranslateY(newY);
-    		this.setCenterY(y);
     	}
     }
     
@@ -152,28 +182,28 @@ public class UI extends Application {
 	}
 	
 	static class Drone extends CoordinateShape {
-		static final Color c = Color.BLUE;		
+		static final Paint c = Color.BLUE;		
 		public Drone(GridDisplay gridDisplay, int x, int y){
 			super(gridDisplay, x, y, c);
 		}
 	}
 	
 	static class Tree extends CoordinateShape{
-		static final Color c = Color.GREEN;
+		static final Paint c = Color.GREEN;
 		public Tree(GridDisplay gridDisplay, int x, int y){
 			super(gridDisplay, x, y, c);
 		}
 	}
 	
 	static class Gold extends CoordinateShape {
-		static final Color c = Color.GOLD;
+		static final Paint c = Color.GOLD;
 		public Gold(GridDisplay gridDisplay, int x, int y){
 			super(gridDisplay, x, y, c);
 		}
 	}
     
     static class Box extends Rectangle {
-		static final Color c = Color.BLACK;		
+		static final Paint c = Color.BLACK;		
 		public Box(double sizeN){
 			super(sizeN, sizeN, c);			
 		}
